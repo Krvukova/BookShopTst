25c45c04f9995d39fd3aab6a1b693191f9a3e28c
krvukova <krvukova@vsite.hr>
2025-01-10 19:38:05 +0100

    prvi dio

---
+++ Models/Entities/Book.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BookShopTest.Models.Entities
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        [Precision(18, 2)]
+        public decimal Price { get; set; }
+        public string Genre { get; set; }
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,3 +1,6 @@
+using BookShopTest.Data;
+using Microsoft.EntityFrameworkCore;
+
 namespace BookShopTest
 {
     public class Program
@@ -8,6 +11,9 @@ namespace BookShopTest
 
             // Add services to the container.
             builder.Services.AddControllersWithViews();
+
+            builder.Services.AddDbContext<ApplicationDbContext>(options => 
+            options.UseSqlServer(builder.Configuration.GetConnectionString("BookPortal")));
 
             var app = builder.Build();
 
