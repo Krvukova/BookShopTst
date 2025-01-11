22313ada57d2b1d56ee1144406371e81988caea7
krvukova <krvukova@vsite.hr>
2025-01-11 12:51:26 +0100

    add/save

---
+++ Controllers/BooksController.cs
@@ -0,0 +1,40 @@
+﻿using BookShopTest.Data;
+using BookShopTest.Models;
+using BookShopTest.Models.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookShopTest.Controllers
+{
+    public class BooksController : Controller
+
+    {
+        private readonly ApplicationDbContext dbContext;
+
+        public BooksController(ApplicationDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task <IActionResult> Add(AddBookViewModel viewModel)
+        {
+            var book = new Book
+            {
+                Title = viewModel.Title,
+                Author = viewModel.Author,
+                Price = viewModel.Price,
+                Genre = viewModel.Genre,
+            };
+            await dbContext.Books.AddAsync(book);
+            await dbContext.SaveChangesAsync();
+
+
+            return View();
+
+        }
+    }
+}
---
+++ Models/AddBookViewModel.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BookShopTest.Models
+{
+    public class AddBookViewModel
+    {
+        public string Title { get; set; }
+        public string Author { get; set; }
+        [Precision(18, 2)]
+        public decimal Price { get; set; }
+        public string Genre { get; set; }
+    }
+}
