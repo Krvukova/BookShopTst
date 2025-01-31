8ed4f579eab4eebcb34d354f2efa477b693bd282
krvukova <krvukova@vsite.hr>
2025-01-31 13:31:45 +0100

    popravak - Resend email confirmation

---
+++ Areas/Identity/Pages/Account/ResendConfirmation.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BookShopTest.Areas.Identity.Pages.Account
+{
+    public class ResendConfirmationModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
